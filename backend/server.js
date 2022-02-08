bcdd9a6c20dbcc2821034d831fb26b036b8742d3
bkeuria1 <benkeurian@gmail.com>
2022-02-07 20:51:12 -0500

    Production

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,11 @@ app.use(cors({
 }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, 
+  { useNewUrlParser: true 
+  ,useUnifiedTopology: true,
+  
+  })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
